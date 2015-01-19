43a29a79678a5d26cf442573220a67a320f7d242
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-19 13:29:53 -0500

    some scaling stuff

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-var svg = d3.select("body").append("svg")
-      .attr("width", window.innerWidth)
-      .attr("height", window.innerHeight);
+var w = window.innerWidth,
+    h = window.innerHeight,
+    x_scale = d3.scale.linear().range([0, w]),
+    // SVG indexes the screen like a matrix
+    y_scale = d3.scale.linear().range([h, 0]),
+    x_val = function(d) { return d.x; },
+    y_val = function(d) { return d.y; },
+    x_pos = function(d) { return x_scale(x_val(d)); },
+    y_pos = function(d) { return y_scale(y_val(d)); },
+    svg = d3.select("body").append("svg")
+      .attr("width", w)
+      .attr("height", h);
 
 function draw(data) {
+  matchScaleToData(x_scale, x_val);
+  matchScaleToData(y_scale, y_val);
+
   var points = svg.selectAll(".point").data(data);
+  points.selectAll("title").remove();
+  points.append("title")
+    .text(function(d) { return JSON.stringify(d, null, 2); });
   points.transition()
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-  points.enter()
-    .append("circle")
-    .attr("class", "point")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
+    .attr("cx", x_pos)
+    .attr("cy", y_pos);
+
+  var new_circles = points.enter().append("circle");
+  new_circles.attr("class", "point")
+    .attr("cx", x_pos)
+    .attr("cy", y_pos)
     .attr("r", 0)
     .transition()
     .attr("r", 4);
+  new_circles.append("title")
+    .text(function(d) { return JSON.stringify(d, null, 2); });
+
   points.exit()
     .transition()
     .attr("r", 0)
     .remove();
+
+  function matchScaleToData(scale, fieldFunction) {
+    var minimum = d3.min(data, fieldFunction),
+        maximum = d3.max(data, fieldFunction);
+    scale.domain([minimum, maximum]);
+  }
 }
+
 
 var socket = new WebSocket("ws://localhost:4808/data");
 socket.onmessage = function(event) {
