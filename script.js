42d388c69f63659ca560d70181e5c13e1e09fe6b
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-19 12:33:34 -0500

    move points around

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var svg = d3.select("body").append("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight);
+      .attr("width", window.innerWidth)
+      .attr("height", window.innerHeight);
 
 function draw(data) {
-  svg.selectAll(".point")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "point")
-      .attr("r", 4)
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
+  var points = svg.selectAll(".point").data(data);
+  points.transition()
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
+  points.enter()
+    .append("circle")
+    .attr("class", "point")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", 0)
+    .transition()
+    .attr("r", 4);
+  points.exit()
+    .transition()
+    .attr("r", 0)
+    .remove();
 }
 
 var socket = new WebSocket("ws://localhost:4808/data");
